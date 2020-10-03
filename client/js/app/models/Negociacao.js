7814ddebf535e72b348dee932f561b827ec61ab4
Taiany <braziltaiany@outlook.com>
2020-10-03 09:12:29 -0300

    teste com interacao de dado e comportamento

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,7 +1,10 @@
 class Negociacao {
-  constructor() {
-    this.data = new Date();
-    this.quantidade = 1;
-    this.valor = 0.0;
+  constructor(data, quantidade, valor) {
+    this.data = data;
+    this.quantidade = quantidade;
+    this.valor = valor;
+  }
+  obtemVolume() {
+    return this.quantidade * this.valor;
   }
 }
