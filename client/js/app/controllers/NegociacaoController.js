fe8609f62aa3ad347433251c2ad400c4e8c214b6
Taiany <braziltaiany@outlook.com>
2020-10-03 14:55:14 -0300

    Criando uma instancia de Negociacao e utilizando Arrow Functions

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,20 +9,18 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-    // let data = new Date(this._inputData.value.replace(/-/g, ',')); //tratar a data com expressão regular
     let data = new Date(
-      ...this._inputData.value.split('-').map(function (item, indice) {
-        // if (indice == 1) {return item - 1;} return item;
-        return item - (indice % 2);
-      })
+      ...this._inputData.value
+        .split('-')
+        .map((item, indice) => item - (indice % 2))
     );
 
-    console.log(data);
-
-    /*  let negociacao = new Negociacao(
-      new Date(this._inputData.value.split('-')),
+    let negociacao = new Negociacao(
+      data,
       this._inputQuantidade.value,
       this._inputValor.value
-    ); */
+    );
+
+    //console.log(negociacao);
   }
 }
