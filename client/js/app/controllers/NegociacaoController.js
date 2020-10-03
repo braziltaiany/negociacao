3a20d8d588a685cf07fac74f75ae24c081bb62d0
Taiany <braziltaiany@outlook.com>
2020-10-03 13:08:28 -0300

    captura dos dados preenchidos no formulario

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,14 @@
 class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
-    alert('chamei ação no controller');
+
+    let $ = document.querySelector.bind(document);
+    let inputData = $('#data');
+    let inputQuantidade = $('#quantidade');
+    let inputValor = $('#valor');
+
+    console.log(inputData.value);
+    console.log(inputQuantidade.value);
+    console.log(inputValor.value);
   }
 }
