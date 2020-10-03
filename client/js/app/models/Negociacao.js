f3eefcbbb7cae95475b56f7902952c977419af3a
Taiany <braziltaiany@outlook.com>
2020-10-03 09:32:25 -0300

    convencao  underline (_) - de que nos atributos das propriedades de uma classe não pode ser modificada e metodo acessadores(get)

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,10 +1,22 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-    this.data = data;
-    this.quantidade = quantidade;
-    this.valor = valor;
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
-  obtemVolume() {
-    return this.quantidade * this.valor;
+  getVolume() {
+    return this._quantidade * this._valor;
+  }
+
+  getData() {
+    return this._data;
+  }
+
+  getQuantidade() {
+    return this._quantidade;
+  }
+
+  getValor() {
+    return this._valor;
   }
 }
