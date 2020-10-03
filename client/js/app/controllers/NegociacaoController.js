b7b694347b364f0361f665bd3ad9744962ce1db5
Taiany <braziltaiany@outlook.com>
2020-10-03 13:15:51 -0300

    captura mais performatica dos dados preenchidos no formulario

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,14 +1,16 @@
 class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document);
+
+    this.inputData = $('#data');
+    this.inputQuantidade = $('#quantidade');
+    this.inputValor = $('#valor');
+  }
   adiciona(event) {
     event.preventDefault();
 
-    let $ = document.querySelector.bind(document);
-    let inputData = $('#data');
-    let inputQuantidade = $('#quantidade');
-    let inputValor = $('#valor');
-
-    console.log(inputData.value);
-    console.log(inputQuantidade.value);
-    console.log(inputValor.value);
+    console.log(this.inputData.value);
+    console.log(this.inputQuantidade.value);
+    console.log(this.inputValor.value);
   }
 }
