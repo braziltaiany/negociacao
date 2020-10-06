0702eb7f8e04196db63871d1d32b3afcf6930033
Taiany <braziltaiany@outlook.com>
2020-10-06 14:42:30 -0300

    utilizacao de metodo estatico na classe DateHelper

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,12 +1,16 @@
 class DateHelper {
+  constructor() {
+    //DateHelper não pode ser instanciada
+    throw new Error('Esta classe não pode ser instanciada');
+  }
   //metodo 1
-  dataParaTexto(data) {
+  static dataParaTexto(data) {
     return `${data.getDate()}${'/'}${
       data.getMonth() + 1
     }${'/'}${data.getFullYear()}`;
   }
   //metodo 2
-  textoParaData(texto) {
+  static textoParaData(texto) {
     return new Date(
       ...texto.split('-').map((item, indice) => item - (indice % 2))
     );
